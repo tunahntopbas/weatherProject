aefe81fe7dceca483fa7d02ae7acec9cf2cce2ea
VELI TUNAHAN TOPBAS <topikbas@icloud.com>
2026-07-29 08:27:08 +0300

    chore: backend clean architecture solution iskeleti

---
+++ backend/tests/WeatherProject.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherProject.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ backend/tests/WeatherProject.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherProject.Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
